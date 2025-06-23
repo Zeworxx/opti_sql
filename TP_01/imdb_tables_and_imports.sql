8e7b00ffad46ca1293a0d79da86de29217e5eb67
Baptiste <ba.girard@ttge.fr>
2025-06-23 12:27:20 +0200

    Adding queries from TP

--- TP_01/imdb_tables_and_imports.sql
+++ TP_01/imdb_tables_and_imports.sql
@@ -95,3 +95,26 @@ WITH (FORMAT csv, DELIMITER E'\t', HEADER, NULL '\N', QUOTE E'\001');
 
 
 -- 8. Execute queries in TP_01
+
+-- Comptage simple
+SELECT COUNT(*) FROM title_basics;
+-- Distribution par type
+SELECT title_type, COUNT(*)
+FROM title_basics
+GROUP BY title_type
+ORDER BY COUNT(*) DESC;
+-- Films les mieux notés
+SELECT b.primary_title, r.average_rating, r.num_votes
+FROM title_basics b
+JOIN title_ratings r ON b.tconst = r.tconst
+WHERE b.title_type = 'movie'
+ORDER BY r.average_rating DESC
+LIMIT 10;
+
+EXPLAIN ANALYZE
+SELECT b.primary_title, r.average_rating
+FROM title_basics b
+JOIN title_ratings r ON b.tconst = r.tconst
+WHERE r.num_votes > 1000
+ORDER BY r.average_rating DESC
+LIMIT 10;
